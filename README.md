192662a654bfaa85c45c811f25734772d7462b93
Shuvkant <weird426344@gmail.com>
2024-10-27 15:50:30 +0545

    linking frontend and backend

---
+++ README.md
@@ -0,0 +1,9 @@
+## This is fun project to learn the linking of backend and frontend
+
+### Frontend
+
+There is the use of proxy in the frontend part.
+
+### Backend
+
+The server runs in localhost and the default port is 3000
--- backend/server.js
+++ backend/server.js
@@ -3,10 +3,20 @@ import express from "express";
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Shuvkant Chaudhary");
+  res.send("Shuvkant Chaudhary Phanait");
 });
 
-// const port = process.env.PORT || 3000;
+app.get("/api/jokes", (req, res) => {
+  const jokes = [
+    { id: 1, title: "first jokes", content: "This is first joke" },
+    { id: 2, title: "second jokes", content: "This is second joke" },
+    { id: 3, title: "third joke", content: "This is third joke" },
+    { id: 4, title: "fourth joke", content: "this is fourth joke" },
+  ];
+  res.send(jokes);
+});
+
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`serve at https://localhost:${port}`);
