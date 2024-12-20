f2671a224f59fa66d7cc3a0ea27ccfed0fd10876
Shuvkant <weird426344@gmail.com>
2024-12-20 22:29:34 +0545

    allowing all origin

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors"
 
 const app = express();
+
 app.use(cors())
 
 
@@ -15,6 +16,7 @@ app.get("/api/jokes", (req, res) => {
     { id: 2, title: "second joke", content: "I’m reading a book about anti-gravity. It’s impossible to put down" },
     { id: 3, title: "third joke", content: "Parallel lines have so much in common. It’s a shame they’ll never meet" },
     { id: 4, title: "fourth joke", content: "I told my computer I needed a break. Now it won’t stop sending me KitKats." },
+    { id: 5, title: "third joke", content: "Why don’t scientists trust atoms? Because they make up everything." }
   ];
   res.send(jokes);
 });
