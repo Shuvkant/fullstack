ed4001e6ba6792da2cb2f9176347752acc89ff89
Shuvkant <weird426344@gmail.com>
2024-12-20 21:47:12 +0545

    addes cors

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,20 @@
 import express from "express";
+import cors from "cors"
 
 const app = express();
+app.use(cors())
+
 
 app.get("/", (req, res) => {
-  res.send("Shuvkant ");
+  res.send("Shuvkant Implemented Cors ");
 });
 
 app.get("/api/jokes", (req, res) => {
   const jokes = [
     { id: 1, title: "first jokes", content: "Why don’t skeletons fight each other? They don’t have the guts" },
-    { id: 2, title: "second jokes", content: "This is second joke" },
-    { id: 3, title: "third joke", content: "This is third joke" },
-    { id: 4, title: "fourth joke", content: "this is fourth joke" },
+    { id: 2, title: "second joke", content: "I’m reading a book about anti-gravity. It’s impossible to put down" },
+    { id: 3, title: "third joke", content: "Parallel lines have so much in common. It’s a shame they’ll never meet" },
+    { id: 4, title: "fourth joke", content: "I told my computer I needed a break. Now it won’t stop sending me KitKats." },
   ];
   res.send(jokes);
 });
