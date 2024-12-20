614cefd8b4fbceba76430d2a5d70a65ce372b41d
Shuvkant <weird426344@gmail.com>
2024-12-20 21:48:59 +0545

    fetching jokes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -17,8 +17,7 @@ function App() {
       <p>Jokes: {jokes.length}</p>
       {jokes.map((joke, index) => (
         <div key={joke.id}>
-          <h3>{joke.title}</h3>
-          <p>{joke.content}</p>
+          <p>{joke.id}) {joke.content}</p>
         </div>
       ))}
     </>
